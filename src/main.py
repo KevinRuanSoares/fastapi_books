845dba50d46c4ea4005d3a3389f6f0f3234ba0d3
Kevin Ruan Soares <kevin.soares@azos.com.br>
2023-08-23 09:41:24 -0300

    chore: added new create book endpoint;

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI  
+from fastapi import FastAPI, Body
 app = FastAPI()
 
 BOOKS = [
@@ -27,3 +27,17 @@ async def read_category_by_query(category: str):
     if book.get('category').casefold() == category.casefold():
       books_to_return.append(book)
   return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+  books_to_return = []
+  for book in BOOKS:
+    if book.get('author').casefold() == book_author.casefold() and \
+      book.get('category').casefold() == category.casefold():
+      books_to_return.append(book)
+  return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+  BOOKS.append(new_book)
+  
